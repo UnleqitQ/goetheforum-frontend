c02bfc0f0bbcd39244b9d8bd6a809ba73bd3c61d
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-06-17 20:49:45 +0200

    add i18n to page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import i18n from './i18n';
 import {createHashRouter, RouterProvider} from "react-router-dom";
 import {APIOptions, PrimeReactProvider} from "primereact/api";
 import {PageShell} from "./pages/shell/PageShell.tsx";
+import {I18nextProvider} from "react-i18next";
 
 const router = createHashRouter([
 	{
@@ -30,10 +32,11 @@ const primeReactConfig: Partial<APIOptions> = {
 
 root.render(
 	<React.StrictMode>
-		<PrimeReactProvider value={primeReactConfig}>
-			
-			<RouterProvider router={router} />
-		</PrimeReactProvider>
+		<I18nextProvider i18n={i18n}>
+			<PrimeReactProvider value={primeReactConfig}>
+				<RouterProvider router={router} />
+			</PrimeReactProvider>
+		</I18nextProvider>
 	</React.StrictMode>
 );
 
