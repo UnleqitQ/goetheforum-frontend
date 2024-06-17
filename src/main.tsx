4130fbc18c161c5ad41070276ae9cbb15c94c8dd
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-06-17 17:21:49 +0200

    Remove app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import {createHashRouter, RouterProvider} from "react-router-dom";
 import {APIOptions, PrimeReactProvider} from "primereact/api";
@@ -8,7 +7,7 @@ import {APIOptions, PrimeReactProvider} from "primereact/api";
 const router = createHashRouter([
 	{
 		path: '/',
-		element: <App />
+		element: <div>Home</div>
 	}
 ], {});
 
